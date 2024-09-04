97997139c76c672c9535973495fa780d5d85da89
nirav-rafay <93963752+nirav-rafay@users.noreply.github.com>
2024-09-04 17:03:29 +0530

    Update variables.tf

--- null-provider/variables.tf
+++ null-provider/variables.tf
@@ -1,19 +1,19 @@
 variable "itest1" {
   description = "test 1"
-  default     = "default-test1"
+  default     = "default-test1-value"
 }
 
 variable "itest2" {
   description = "test 2"
-  default     = "default-test2"
+  default     = "default-test2-value"
 }
 
 variable "itest3" {
   description = "test 3"
-  default     = "default-test3"
+  default     = "default-test3-value"
 }
 
 variable "itest4" {
   description = "test 4"
-  default     = "default-test4"
+  default     = "default-test4-value"
 }
